b67c7e4318c768c66dd4ebbf015a500a46793fe6
jihazard <yoonjh238@gmail.com>
2019-04-24 17:42:29 +0900

    배경화면 추가

---
+++ clockTodoList/bg.js
@@ -0,0 +1,30 @@
+
+
+
+const body = document.querySelector("body");
+const IMG_NUMBER = 5;
+
+function handleImageLoad(event){
+    console.log("finished image load")
+}
+
+function paintImage(imgNumber){
+    const image= new Image();
+    image.src =`img/${imgNumber+1}.jpg`;
+    image.classList.add("bgImage")
+    body.prepend(image)
+    
+}
+
+function getRandom(){
+    const number = Math.floor(Math.random() * IMG_NUMBER)
+    return number;
+}
+
+function init(){
+    const randomNumber = getRandom();
+    paintImage(randomNumber)
+
+}
+
+init();
