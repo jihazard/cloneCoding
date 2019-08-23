01f97b9e8193b4995ae92a34d37a0b715221e6c8
jihazard <yoonjh238@gmail.com>
2019-08-23 18:06:02 +0900

    mongooes

--- twitter/server/index.js
+++ twitter/server/index.js
@@ -2,7 +2,18 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require(`body-parser`)
+const monk = require('monk')
+const mongoose = require('mongoose');
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://psyche2823:aaaaaaaa@cluster0-yov3s.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("접속")
+  client.close();
+});
 
 app.use(cors());
 app.use(express.json())
